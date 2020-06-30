0ebdeaf0e4d6b6064366e528b907c6379cef1e1d
KonradPietocha <konrad.pietocha@gazeta.pl>
2020-06-30 16:11:37 +0200

    start weather-project

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import LocalWeather from './components/LocalWeather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default LocalWeather;
---
+++ components/LocalWeather.js
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Platform, TextInput } from 'react-native';
+import Forecast from './Forecast';
+
+export default function LocalWeather() {
+    const [state, setState] = useState({
+        code: "",
+        forecast: null
+    });
+    const handleTextChange = event => {
+        setState(state => ({ ...state, code: event.nativeEvent.text }));
+    };
+    let content = null;
+    if (state.forecast !== null) {
+        content = (
+            <Forecast 
+                main={state.forecast.main}
+                description={state.forecast.description}
+                temp={state.forecast.temp}
+            />
+        );
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>
+                You entered {state.code}.
+            </Text>
+            {content}
+            <TextInput 
+                style={styles.input}
+                onSubmitEditing={handleTextChange}
+            />
+            <Text style={styles.instructions}>
+                {instructions}
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#9acd32',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    },
+    input: {
+        fontSize: 20,
+        borderWidth: 2,
+        padding: 2,
+        height: 40,
+        width: 100,
+        textAlign: 'center'
+    }
+});
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' +
+        'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu'
+});
